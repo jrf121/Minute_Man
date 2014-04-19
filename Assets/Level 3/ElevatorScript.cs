ee13466464e002950fd44955acc2dfb3b397ea2e
GastricBypass <dsp45@case.edu>
2014-04-19 18:46:10 -0400

    2Minute
    Changes to level 3

---
+++ Assets/Level 3/ElevatorScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorScript : MonoBehaviour {
+
+	private bool inRange;
+	public GameObject elevator;
+	//public GameObject platformRiser2;
+	
+	void Start () {
+	}
+	
+	void OnTriggerStay2D () {
+		inRange = true;
+	}
+	void OnTriggerExit2D () {
+		inRange = false;
+	}
+	
+	void Update () {
+		if (inRange && Input.GetKeyDown(KeyCode.UpArrow)) {
+			GameObject.Find("Minute Man").transform.position=new Vector3(elevator.transform.position.x,elevator.transform.position.y,-1);
+			//platformRiser2.GetComponent<RiserScript>().extend();
+		}
+	}
+}
